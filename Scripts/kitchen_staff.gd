0033c691bb677f2f1c6355679176689d980b0881
arashi1214 <opp40519@gmail.com>
2026-01-31 13:39:16 +0800

    udpate: 重構後台廚師

--- Scripts/kitchen_staff.gd
+++ Scripts/kitchen_staff.gd
@@ -15,45 +15,18 @@ var prepare_time: float = 2.0
 func _ready():
 	game_manager = get_tree().get_first_node_in_group("game_manager")
 	
-	# 根據 staff_id 設置可用食物
-	if staff_id == 1:
-		available_foods = [
-			GameManager.FoodType.BEEF_NOODLE,
-			GameManager.FoodType.STINKY_TOFU,
-			GameManager.FoodType.PEARL_MILK_TEA
-		]
-		desired_expression = GameManager.ExpressionType.HAPPY
-	else:
-		available_foods = [
-			GameManager.FoodType.OYSTER_OMELETTE,
-			GameManager.FoodType.BRAISED_PORK
-		]
-		desired_expression = GameManager.ExpressionType.NEUTRAL
-
-func can_prepare_food(food_type: GameManager.FoodType) -> bool:
-	return food_type in available_foods
 
 func start_preparing(food_type: GameManager.FoodType):
-	if not can_prepare_food(food_type):
-		return false
-	
-	is_preparing = true
-	prepare_timer = 0.0
-	prepare_time = 2.0  # 準備時間
+	$CookTimer.wait_time = prepare_time
+	$CookTimer.start()
 	return true
-
-func _process(delta):
-	if is_preparing:
-		prepare_timer += delta
-		if prepare_timer >= prepare_time:
-			is_preparing = false
-			prepare_timer = 0.0
-
-func is_ready() -> bool:
-	return not is_preparing
 
 func receive_expression(expression: GameManager.ExpressionType):
 	if expression == desired_expression:
 		mood = min(max_mood, mood + 10)
 	else:
 		mood = max(0, mood - 5)
+
+func cook_finish():
+	print("煮好了")
+	return
