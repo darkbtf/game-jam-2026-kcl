e90683fb5e7f4c60c343f27d1809ecaf48f56e0f
arashi1214 <opp40519@gmail.com>
2026-02-01 14:58:05 +0800

    door 狀態調整

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -4,3 +4,8 @@ extends AnimatedSprite2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		play("open")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		play("close") # Replace with function body.
